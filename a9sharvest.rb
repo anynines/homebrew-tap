c152be7eca9f32f6a0eec437659e42a2ad0a2f0d
Robert Gogolok <gogolok@gmail.com>
2020-09-28 11:52:05 +0200

    a9sharvest 1.6.0

--- a9sharvest.rb
+++ a9sharvest.rb
@@ -3,11 +3,11 @@ require 'formula'
 class A9sharvest < Formula
   desc "a9sharvest"
   homepage 'https://github.com/anynines/a9sharvest'
-  version 'v1.5.0'
+  version 'v1.6.0'
 
   if OS.mac?
-    url 'https://github.com/anynines/a9sharvest/releases/download/v1.5.0/a9sharvest-macos'
-    sha256 '32c11f26f370e117d971bf095f416cada4a17dfbf17690d5d269a2d368a5cfe4'
+    url 'https://github.com/anynines/a9sharvest/releases/download/v1.6.0/a9sharvest-macos'
+    sha256 '455ce1fc68906feae8c4f806e730c62b5b610ff75ecaa1101e0ac5e203ea5b92'
   end
 
   depends_on :arch => :x86_64
